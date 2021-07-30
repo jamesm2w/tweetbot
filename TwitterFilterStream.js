32b093a314e624c7afb2131605cc7a16d4ac1484
jamesm2w <jamesmacerwright@gmail.com>
2021-07-30 12:50:19 +0100

    Added express + methods. Moved twitter code into new class.

---
+++ TwitterFilterStream.js
@@ -0,0 +1,187 @@
+const needle = require("needle");
+
+const rulesURL = 'https://api.twitter.com/2/tweets/search/stream/rules';
+const streamURL = 'https://api.twitter.com/2/tweets/search/stream?expansions=author_id&user.fields=name,username,profile_image_url,id&tweet.fields=author_id,id';
+const token = process.env.BEARER_TOKEN;
+
+class TwitterFilterStream {
+
+    _constructor (onDataFunction, filterRules) {
+        console.log(filterRules);
+        this.onDataFunction = onDataFunction;
+        this.filterRules = filterRules;
+        this.lastAlive = undefined;
+
+        this.stream = undefined;
+        this.connected = false;
+    }
+
+    async connect () {
+        this.log("Attempting to connect to twitter stream.")
+        try {
+
+            this.log("[RuleLoader] Loaded rules: " + JSON.stringify(this.filterRules));
+
+            // Gets the complete list of rules currently applied to the stream
+            let currentRules = await this.getCurrentRules();
+            this.log("[RuleLoader] Current Stream Rules: " + JSON.stringify(currentRules));
+
+            // Delete all rules. Comment the line below if you want to keep your existing rules.
+            await this.deleteRules(currentRules);
+            this.log("[RuleLoader] Cleared Rules");
+
+            // Add rules to the stream. Comment the line below if you don't want to add new rules.
+            await this.setRules(this.filterRules);
+
+            // Check if rules have been applied correctly
+            this.log("[RuleLoader] Final Stream Rules: " + JSON.stringify(await getCurrentRules()));
+        } catch (err) {
+            // Promise Errors get thrown up here and it exits with non-0 code.
+            console.error(err);
+            console.log(JSON.stringify(err));
+            process.exit(1);
+        }
+
+        this.stream = this.twitterStreamConnect(0);
+    }
+
+    setConnected (state) {
+        this.log("Connected State Changed to " + state);
+        this.connected = state;
+    }
+
+    destroyStream () {
+        if (this.connected && this.stream != undefined) {
+            this.stream.destroy();
+            this.setConnected(false);
+        }
+    }
+
+    twitterStreamConnect (retryAttempt) {
+        const stream = needle.get(streamURL, {
+            headers: {
+                "User-Agent": "tweetbotFilterStream",
+                "Authorization": `Bearer ${token}`
+            },
+            timeout: 20000
+        });
+        
+        stream.on("data", async data => {
+            try {
+                const json = JSON.parse(data);
+                console.log(json);
+                this.onDataFunction(json);
+                
+                // Successful connection => reset retry counter
+                retryAttempt = 0;
+            } catch (e) {
+                if (data.detail === "This stream is currently at the maximum allowed connection limit.") {
+                    // Twitter stream limit - maybe previous connection has not wrapped up?
+                    // Kill signal
+                    this.log("[Stream] Twitter Connection Refused");
+                    this.log(data.detail);
+                    setTimeout(() => {
+                        console.warn("[Stream] A connection error occurred. Reconnecting...")
+                        twitterStreamConnect(++retryAttempt);
+                    }, 2 ** retryAttempt);
+                } else {
+                    // Keep alive signal
+                    this.log("[Stream] Keep Alive from Twitter");
+                    this.lastAlive = new Date();
+
+                    if (!this.connected) {
+                        this.setConnected(true);
+                    }
+                }
+            }
+        }).on('err', error => {
+            this.log("[Stream] Error connecting to twitter stream");
+            if (error.code !== 'ECONNRESET') {
+                // Some other connection error
+                this.log(error.code);
+                process.exit(1);
+            } else {
+                // This reconnection logic will attempt to reconnect when a disconnection is detected.
+                // To avoid rate limits, this logic implements exponential backoff, so the wait time
+                // will increase if the client cannot reconnect to the stream. 
+                setTimeout(() => {
+                    console.warn("[Stream] A connection error occurred. Reconnecting...")
+                    twitterStreamConnect(++retryAttempt);
+                }, 2 ** retryAttempt);
+            }
+        });
+    
+        return stream;
+    }
+
+    async getCurrentRules () {
+        let response = await needle("GET", rulesURL, {}, {
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        });
+    
+        if (response.statusCode !== 200) {
+            this.log("[Get Rules] Err: " + response.statusCode + " " + response.statusMessage);
+            throw new Error(response.body);
+        }
+    
+        return response.body;
+    }
+
+    async deleteRules (ruleArr) {
+        if (!Array.isArray(ruleArr)) {
+            return null;
+        }
+        console.log(ruleArr);
+        let ids = ruleArr.data.map(rule => rule.id);
+        console.log(ids);
+        let data = {
+            "delete": {
+                "ids": ids
+            }
+        };
+    
+
+        let response = await needle("POST", rulesURL, data, {
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            }
+        });
+    
+        if (response.statusCode !== 200) {
+            this.log("[Delete Rules] Err: " + response.statusCode + " " + response.statusMessage);
+            throw new Error(response.body);
+        }
+    
+        return response.body;
+    }
+
+    async setRules (rules) {
+        let data = {
+            "add": rules
+        };
+    
+        let response = await needle('POST', rulesURL, data, {
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            }
+        })
+    
+        if (response.statusCode !== 201) {
+            this.log("[Set Rules] Err: " + response.statusCode + " " + response.statusMessage);
+            throw new Error(response.body);
+        }
+    
+        return response.body;
+    }
+
+    log (msg) {
+        let date = (new Date()).toLocaleString("en-GB");
+        console.log("[" + date + "][TwitterFilterStream] " + msg);
+    }
+}
+
+module.exports = TwitterFilterStream;
--- index.js
+++ index.js
@@ -1,206 +1,166 @@
 require("dotenv").config();
-const { MongoClient } = require('mongodb');
-const http = require("http");
+
+const { MongoClient } = require("mongodb");
+const TwitterFilterStream = require("./TwitterFilterStream.js");
+const express = require("express");
 const needle = require("needle");
 
-const token = process.env.BEARER_TOKEN;
 const dbClient = new MongoClient(process.env.CONNECTIONSTRING);
 const dbName = "tweetbot";
+const app = express();
 
-//no i wont const webhook = "webhook";
+app.use(express.json());
 
-const rulesURL = 'https://api.twitter.com/2/tweets/search/stream/rules';
-const streamURL = 'https://api.twitter.com/2/tweets/search/stream?expansions=author_id&user.fields=name,username,profile_image_url,id&tweet.fields=author_id,id';
-
-let lastAlive = new Date();
+let listeningHooks = {};
 
 function log (msg) {
     let date = (new Date()).toLocaleString("en-GB");
-    console.log("[" + date + "] " + msg);
+    console.log("[" + date + "][Index] " + msg);
 }
 
-// Fetch current rules from twitter
-async function getCurrentRules () {
-    let response = await needle("GET", rulesURL, {}, {
-        headers: {
-            "Authorization": `Bearer ${token}`
-        }
-    });
-
-    if (response.statusCode !== 200) {
-        log("[Get current rules] Err: " + response.statusCode + " " + response.statusMessage);
-        throw new Error(response.body);
-    }
-
-    return response.body;
-}
-
-// Delete all the given rules from twitter stream
-async function deleteAllRules (ruleArr) {
-    if (!Array.isArray(ruleArr)) {
-        return null;
-    }
-
-    let ids = ruleArr.data.map(rule => rule.id);
-
-    let data = {
-        "delete": {
-            "ids": ids
-        }
-    };
-
-    let response = await needle("POST", rulesURL, data, {
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${token}`
-        }
-    });
-
-    if (response.statusCode !== 200) {
-        log("[Delete rules] Err: " + response.statusCode + " " + response.statusMessage);
-        throw new Error(response.body);
-    }
-
-    return response.body;
-}
-
-// Set the given rules on the twitter filter stream
-async function setRules (rules) {
-    let data = {
-        "add": rules
-    };
-
-    let response = await needle('POST', rulesURL, data, {
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${token}`
-        }
-    })
-
-    if (response.statusCode !== 201) {
-        log("[Set rules] Err: " + response.statusCode + " " + response.statusMessage);
-        throw new Error(response.body);
-    }
-
-    return response.body;
-}
-
-// Connect to twitter stream and start listening to data
-function twitterStreamConnect (retryAttempt) {
-    const stream = needle.get(streamURL, {
-        headers: {
-            "User-Agent": "v2FilterStreamTEST",
-            "Authorization": `Bearer ${token}`
-        },
-        timeout: 20000
-    });
+async function sendWebhookData (json) {
+    // Construct webhook payload data. Find the user which tweeted in the `include` section
+    // Construct url from base + tweet ID
+    let userData = json.includes.users.find(usr => usr.id == json.data.author_id) || {};
+    let url = `https://twitter.com/${userData.username}/status/${json.data.id}`;
     
-    stream.on("data", async data => {
-        try {
-            const json = JSON.parse(data);
+    // Provide some fallbacks just in-case data from twitter is not available.
+    let hookLoad = {
+        "username": userData.name || "Twitter Bot",
+        "avatar_url": userData.profile_image_url || "", 
+        "content": url || "Webhook Error"
+    };
+    
+    // If tweet matched any rules lets send it to those webhooks.
+    for (let hook of listeningHooks[userData.username]) {
+        // Send discord webhook
+        let response = await needle("POST", hook, hookLoad);
 
-            console.log(json);
-
-            // Construct webhook payload data. Find the user which tweeted in the `include` section
-            // Construct url from base + tweet ID
-            let userData = json.includes.users.find(usr => usr.id == json.data.author_id) || {};
-            let url = "https://twitter.com/i/status/" + json.data.id;
-            
-            // Provide some fallbacks just in-case data from twitter is not available.
-            let hookLoad = {
-                "username": userData.name || "Twitter Bot",
-                "avatar_url": userData.profile_image_url || "", 
-                "content": url || "Webhook Error"
-            };
-            
-            // If tweet matched any rules lets send it to those webhooks.
-            for (let rule of json.matching_rules) {
-                // Send discord webhook
-                let response = await needle("POST", rule.tag, hookLoad);
-
-                // Discord responds with 204 No Content when successful.
-                if (response.statusCode !== 204) {
-                    log("[Discord Webhook] unexpected code: " + response.statusCode + " " + response.statusMessage);
-                    throw new Error(response.body);
-                } else {
-                    log("Recieved New Tweet: " + url);
-                }
-            }
-
-            // Successful connection => reset retry counter
-            retryAttempt = 0;
-        } catch (e) {
-            if (data.detail === "This stream is currently at the maximum allowed connection limit.") {
-                // Twitter stream limit - maybe previous connection has not wrapped up?
-                // Kill signal
-                log("[Stream] Twitter Connection Refused");
-                log(data.detail);
-                process.exit(1);
-            } else {
-                // Keep alive signal
-                log("[Stream] Keep Alive from Twitter");
-                lastAlive = new Date();
-            }
-        }
-    }).on('err', error => {
-        log("[Stream] Error connecting to twitter stream");
-        if (error.code !== 'ECONNRESET') {
-            // Some other connection error
-            log(error.code);
-            process.exit(1);
+        // Discord responds with 204 No Content when successful.
+        if (response.statusCode !== 204) {
+            log("[Discord Webhook] unexpected code: " + response.statusCode + " " + response.statusMessage);
+            throw new Error(response.body);
         } else {
-            // This reconnection logic will attempt to reconnect when a disconnection is detected.
-            // To avoid rate limits, this logic implements exponential backoff, so the wait time
-            // will increase if the client cannot reconnect to the stream. 
-            setTimeout(() => {
-                console.warn("[Stream] A connection error occurred. Reconnecting...")
-                streamConnect(++retryAttempt);
-            }, 2 ** retryAttempt)
+            log("[Discord Webhook] Sent New Message: " + url);
         }
-    });
-
-    return stream;
+    }
 }
 
-(async () => {
-    //let rules = [{
-    //    "value": "from:BritainElects OR from:PoliticsForAlI",
-    //    "tag": "webhook"
-    //}];
-
-    await dbClient.connect();
-
-    log("Connected to MongoDB");
-
-    const db = dbClient.db(dbName);
-    const collection = db.collection("rules");
-
-    let rules1 = (await collection.find({}).toArray()).map(r => { return {"value": r.value, "tag": r.tag} });
-    log("Loaded rules: " + JSON.stringify(rules1));
-
-    // Set up the rules for the filtered stream
-    try {
-        // Gets the complete list of rules currently applied to the stream
-        let currentRules = await getCurrentRules();
-
-        // Delete all rules. Comment the line below if you want to keep your existing rules.
-        await deleteAllRules(currentRules);
-
-        // Add rules to the stream. Comment the line below if you don't want to add new rules.
-        await setRules(rules1);
-
-    } catch (e) {
-        console.error(e);
-        process.exit(1);
+function createRuleSet (channels) {
+    let accounts = [];
+    // Loop through every listening channel
+    for (let channel of channels) {
+        // Loop through every account it listens to
+        for (let account of channel.accounts) {
+            // If the account is not a duplicate
+            if (!accounts.includes(account)) {
+                // Add account to the list of accounts and 
+                // add an association of the account name to the hook
+                accounts.push(account);
+                listeningHooks[account] = [channel.webhook];
+            } else {
+                // Else if it's a duplicate - just add the webhook to the association.
+                listeningHooks[account].push(channel.webhook);
+            }
+        }
     }
 
-    log("Establishing connection with Twitter Feed");
+    // Build a list of rules
+    let rules = [];
+    let ruleStr = "";
+    // For every individual account to listen to
+    for (let account of accounts) {
+        // Add a from: operator to the rule string
+        let ruleStrAddition = `from:${account}`;
+        // If rule is not empty need to prepend the "OR" operator
+        if (ruleStr.length != 0) {
+            ruleStrAddition = " OR " + ruleStrAddition;
+        }
+        // Rules can be up to 512 in length. If adding this account would be 
+        // over, add the current rule and start on the next one.
+        if (ruleStr.length + ruleStrAddition.length > 512) {
+            // Add to next rule
+            rules.push({"value": ruleStr, "tag": `Rule ${rules.length+1}`});
+            ruleStr = ruleStrAddition;
+        } else {
+            // Concat rules together
+            ruleStr += ruleStrAddition;
+        }
+    }
 
-    // Listen to the stream.
-    twitterStreamConnect(0);
+    // Add remainder of the accounts to the final rule
+    if (ruleStr.length != 0) {
+        rules.push({"value": ruleStr, "tag": `Rule ${rules.length+1}`});
+    }
 
-    http.createServer((req, res) => {
-        res.writeHead(200);
-        res.end("TweetBot Active and Listening. Last Alive: " + lastAlive.toUTCString());
-    }).listen(8888);
-})();
+    // There can be a maximum of 25 rules on one stream - if we have more than 25 rules ignore overflow
+    // TODO: maybe make something better here.
+    if (rules.length > 25) {
+        log("[RuleConstructor] Error! Too Many Rules. Only applying first 25 rules");
+        rules = rules.slice(0, 24);
+    }
+
+    return rules;
+}
+
+async function run () {
+    try {
+        await dbClient.connect();
+
+        log("Connected to MongoDB");
+
+        const db = dbClient.db(dbName);
+        const channelCollection = db.collection("channels"); 
+
+        let channels = await channelCollection.find({}).toArray();
+        let rules = createRuleSet(channels);
+
+        console.log(rules);
+
+        let twitterManager = new TwitterFilterStream(sendWebhookData, rules);
+        twitterManager.connect();
+
+        // TODO: add express to manage easy add/remove of rules.
+        app.get("/", (req, res) => {
+            let statusStr = "";
+            if (twitterManager.connected) {
+                statusStr = "TweetBot Connected and Listening"
+            } else {
+                statusStr = "TweetBot Disconnected"
+            }
+
+            statusStr += " Last Alive: " + twitterManager.lastAlive.toISOString();
+            res.send(statusStr);
+        });
+
+        app.post("/channels", (req, res) => {
+            // => {webhook: "https://webhook.com/etc", accounts: ["a", "b", "c"]}
+            let rule = req.body;
+            if (rule.webhook == undefined || !Array.isArray(rule.accounts)) {
+                res.status(400);
+                res.send({"success": false});
+            } else {
+                channelCollection.insertOne(rule);
+                res.status(200);
+                res.send({"success": true});
+            }
+        });
+
+        app.get("/channels", async (req, res) => {
+            res.send(await channelCollection.find({}).toArray());
+        });
+
+        app.get("/rules", (req, res) => {
+            res.send(rules);
+        });
+
+        app.listen(process.env.PORT);
+    } finally {
+        dbClient.close();
+    }
+}
+
+run().catch(err => {
+    console.log(err);
+    throw new Error(err);
+});
